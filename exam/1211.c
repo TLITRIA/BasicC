fa3e6148c10e4647f67079332c0bff9d8620541e
TLITRIA <darkfor9es0t@163.com>
2023-12-11 20:18:00 +0800

    [新增] 新增makefile的学习与部分作业

--- exam/1211.c
+++ exam/1211.c
@@ -1,25 +1,94 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_LENGTH 20
+#define ARRAY_RANGE 1000
+
+
+#define ENTER printf("\n")
+
+void printList(int array[])
+{
+    for (int idx = 0; idx < ARRAY_LENGTH; idx++)
+    {
+        printf("%d  ", array[idx]);
+    }
+    printf("\n=========================\n");
+}
+
+void quickSort(int array[], int start, int end)
+{
+    if (start < end)
+    {
+        int base = array[start];
+        int left = start;
+        int right = end;
+
+        while (left < right)
+        {
+            /*** 从最右端索引开始向左找，如果array[right] <= base 
+             * 则将array[right]‘填入’array[left]。
+             * 再从左索引+1开始找，如果array[left] >= base
+             * 则将array[left]‘填入’array[right]。
+             * 如此循环直到left+1=right，此时要不要判断？
+            */
+
+            while ( left < right && array[right] > base )
+            {
+                right--;
+            }
+
+            if ( left < right )
+            {
+                array[left] = array[right];
+                left++;
+            }
+
+            while ( left < right && array[left] < base )
+            {
+                left++;
+            }
+
+            if ( left < right )
+            {
+                array[right] = array[left];
+                right--;
+            }
+
+        }
+        // 出循环时left=right了
+        array[left] = base;
+
+        quickSort(array, start, left - 1);
+        quickSort(array, left + 1, end);
+        
+
+    }
+}
+
 
 int main()
 {
+    srand(time(NULL));
+    int array[ARRAY_LENGTH] = {0};
 
+    for ( int idx = 0; idx < ARRAY_LENGTH; idx++ )
+    {
+        array[idx] = rand() % ARRAY_RANGE;
+    }
 
-/*****************
- * 函数三要素：函数名，参数，返回类型
- * 
- * 
- * 
- * 
- * 
-*/
-
-
+    printList(array);
 
 
 
+    quickSort(array, 0, ARRAY_LENGTH-1);
 
 
+    printList(array);
 
+    return 0;
+}
 
 
 
@@ -29,6 +98,3 @@ int main()
  * [1,1,2,3,4,5,6,7,7,7,7,8,9]
  * [1,2,3,4,5,6,7,8,9]
 */
-    return 0;
-}
-
